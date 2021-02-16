497a0b7d72bc4f0d40fae7d7016afde0d7fd7cbb
tashiad <tashiadavis10@gmail.com>
2021-02-16 11:49:43 -0700

    Create and style Breweries component

---
+++ src/components/Breweries/Breweries.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Breweries.css'
+import BreweryCard from '../BreweryCard/BreweryCard'
+
+const Breweries = ({ breweryData }) => {
+  const cards = breweryData.map(brewery => {
+    return (
+      <BreweryCard
+        id={brewery.id}
+        key={brewery.id}
+        name={brewery.name}
+        type={brewery.brewery_type}
+        city={brewery.city}
+        state={brewery.state}
+        site={brewery.website_url}
+      />
+    )
+  })
+
+  return (
+    <section className="card-container">
+      {cards}
+    </section>
+  )
+}
+
+export default Breweries
